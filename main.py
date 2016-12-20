a758c9749f48f86b52edc0fcae3b26ee5874a3f3
Day <dayannefernandesc@gmail.com>
2016-12-20 14:28:14 -0200

    issue #2

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import time
 import telepot
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 
-chat_id, user_id = (None,None)
+chat_id, user_id, username = (None,None, None)
 
 TOKEN = '293519004:AAFFhIBJrrxYzVs9clx9g2tDpWIi84ZSiio'
 
 def onChatMessage(msg):
-    global user_id, chat_id
+    global user_id, chat_id, username
     content_type, chat_type, chat_id = telepot.glance(msg)
 
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
@@ -21,31 +21,37 @@ def onChatMessage(msg):
 
     if 'new_chat_member' in msg:
         if msg['new_chat_member']['username'] != 'PyLadiesBot':
+            username = msg['new_chat_member']['username']
             user_id = msg['new_chat_member']['id']
             bot.sendMessage(chat_id,
                             'Confirmar usuária que entrou?',
                             reply_markup=keyboard)
 
 def onCallbackQuery(msg):
-    global user_id, chat_id
+    global user_id, chat_id, username
 
     query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
     print 'Callback Query:', query_id, from_id, query_data
 
     if query_data == 'yes':
-        bot.answerCallbackQuery(query_id, text='Confirmada!')
-        bot.sendMessage(chat_id, 'Seja bem vinda PyLady!')
-        user_id = None
+        if user_id:
+            bot.answerCallbackQuery(query_id, text='Confirmada!')
+            bot.sendMessage(chat_id, 'Seja bem vinda PyLady '+ str(username) +'!')
+            user_id, username = (None, None)
+        else:
+            bot.answerCallbackQuery(query_id, text='Erro.')
     else:
         if user_id:
             bot.sendMessage(chat_id, 'Usuário '+
-                        str(user_id) + ' sendo retirado..')
+                        str(username) + ' sendo retirado..')
             bot.kickChatMember(chat_id, user_id)
-            bot.answerCallbackQuery(query_id, text='Usuário retirado.')
+            bot.answerCallbackQuery(query_id, text='Usuário '+ str(username) +' retirado.')
         else:
             bot.answerCallbackQuery(query_id, text='Erro.')
 
 bot = telepot.Bot(TOKEN)
+
+bot.setWebhook()
 
 bot.message_loop({'chat': onChatMessage,
                 'callback_query': onCallbackQuery},
