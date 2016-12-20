d4da2e1e95502272dbbb521ebebe13e06f5632ce
Day <dayannefernandesc@gmail.com>
2016-12-20 02:40:33 -0200

    inline button to accept or deny user, markup notify in private session

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
+# -*- coding: utf-8 -*-
 import sys
 import time
 import telepot
+from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 
 TOKEN = '293519004:AAFFhIBJrrxYzVs9clx9g2tDpWIi84ZSiio'
 
-def handle(msg):
+def onChatMessage(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
-    print content_type, chat_type, chat_id
 
-    if content_type == 'text':
-        bot.sendMessage(chat_id, msg['text'])
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                    [InlineKeyboardButton(text='Confirmar',
+                                        callback_data='yes')],
+                    [InlineKeyboardButton(text='Recusar',
+                                        callback_data='no')]
+                ])
+
+    bot.sendMessage(chat_id,
+                    'Confirmar usuário que entrou?',
+                    reply_markup=keyboard)
+
+def onCallbackQuery(msg):
+    query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
+    print 'Callback Query:', query_id, from_id, query_data
+
+    if query_data == 'yes':
+        bot.answerCallbackQuery(query_id, text='Confirmado!')
+    else:
+        bot.answerCallbackQuery(query_id, text='Usuário retirado.')
 
 bot = telepot.Bot(TOKEN)
-bot.message_loop(handle)
-print 'Listening ...'
-
-while 1:
-    time.sleep(10)
+bot.message_loop({'chat': onChatMessage,
+                'callback_query': onCallbackQuery},
+                run_forever='Listening ...')
